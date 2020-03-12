667828a0d9014ddd32065806e485aa3370e481b5
cunhamuril <murilo.sant@hotmail.com>
2020-03-12 09:03:42 -0300

    feat(app): basic structure started

---
+++ src/index.js
@@ -0,0 +1,53 @@
+const http = require('http');
+const morgan = require('morgan');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const routes = require('./routes');
+
+const server = http.createServer(app);
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Header',
+    'Origin, x-Requrested-Woth, Content-Type, Accept, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).send({});
+  }
+  next();
+});
+
+app.use((req, res, next) => {
+  const erro = new Error('Not found');
+  erro.status = 404;
+  next(erro);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  return res.send({
+    erro: {
+      message: error.message,
+    },
+  });
+});
+
+/**
+ * Routes
+ */
+app.use('/admin', routes);
+
+const port = process.env.PORT || 3001;
+server.listen(port, () => {
+  console.log('Back-end running on port ' + port);
+});
